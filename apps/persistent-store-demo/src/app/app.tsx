e38391d5630dba96a3270451a651435ae4996821
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-06-17 07:25:59 +0200

    Initial commit

---
+++ apps/persistent-store-demo/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="persistent-store-demo" />
+    </div>
+  );
+}
+
+export default App;
